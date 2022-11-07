4086e2dd03e42b2c985ebc7315433dc12f56d068
Amkam <amaduskamara@gmail.com>
2022-11-07 00:57:14 +0000

    Add header, links and styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav className="bg-white w-[1200px] mx-auto shadow-xl rounded-l-xl rounded-r-xl py-5 pl-5 flex items-center justify-between border-t-4 border-cyan-600 mt-3 sticky top-0">
+        <h1 className="text-lg font-semibold">Yo-Blog</h1>
+        <ul className="flex items-center">
+          <Link to="/" className="mx-5">
+            Home
+          </Link>
+          <Link to="/create" className="mx-5">
+            Create
+          </Link>
+          <Link to="/about" className="mx-5">
+            About
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
