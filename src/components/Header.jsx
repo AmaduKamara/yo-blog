e3ab054252293219a0f3fd4750525427cf08a9f8
Amkam <amaduskamara@gmail.com>
2022-11-07 09:15:50 +0000

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,51 +7,50 @@ const Header = ({ active, setActive }) => {
       <nav className="bg-white w-[1200px] mx-auto shadow-xl rounded-l-xl rounded-r-xl py-5 pl-5 flex items-center justify-between border-t-4 border-cyan-600 mt-3 sticky top-0">
         <h1 className="text-lg font-semibold">Yo-Blog</h1>
         <ul className="flex items-center">
-          <Link
-            to="/"
-            className={`mx-5 ${
-              active === "home" ? "text-cyan-500 underline" : ""
-            }`}
-            onClick={() => setActive("home")}
-          >
-            Home
-          </Link>
-          <Link
-            to="/create"
-            className={`mx-5 ${
-              active === "create" ? "text-cyan-500 underline" : ""
-            }`}
-            onClick={() => setActive("create")}
-          >
-            Create
-          </Link>
-          <Link
-            to="/about"
-            className={`mx-5 ${
-              active === "about" ? "text-cyan-500 underline" : ""
-            }`}
-            onClick={() => setActive("about")}
-          >
-            About
-          </Link>
-          <Link
-            to="/auth"
-            className={`mx-5 ${
-              active === "signin" ? "text-cyan-500 underline" : ""
-            }`}
-            onClick={() => setActive("signin")}
-          >
-            Signin
-          </Link>
-          <Link
-            to="/auth"
-            className={`mx-5 ${
-              active === "signup" ? "text-cyan-500 underline" : ""
-            }`}
-            onClick={() => setActive("signup")}
-          >
-            Signup
-          </Link>
+          <li>
+            <Link
+              to="/"
+              className={`mx-5 ${
+                active === "home" ? "text-cyan-500 underline" : ""
+              }`}
+              onClick={() => setActive("home")}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/create"
+              className={`mx-5 ${
+                active === "create" ? "text-cyan-500 underline" : ""
+              }`}
+              onClick={() => setActive("create")}
+            >
+              Create
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              className={`mx-5 ${
+                active === "about" ? "text-cyan-500 underline" : ""
+              }`}
+              onClick={() => setActive("about")}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/auth"
+              className={`mx-5 ${
+                active === "signin" ? "text-cyan-500 underline" : ""
+              }`}
+              onClick={() => setActive("signin")}
+            >
+              Signin
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
