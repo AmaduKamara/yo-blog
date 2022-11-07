afae099021dd3a75c528949904764011b37bebae
Amkam <amaduskamara@gmail.com>
2022-11-07 12:49:53 +0000

    Render username on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaUserAlt } from "react-icons/fa";
 
-const Header = ({ active, setActive }) => {
+const Header = ({ active, setActive, user }) => {
+  const userId = user?.uid;
+  const userName = user?.displayName.split(" ");
+
   return (
     <header>
       <nav className="bg-white w-[1200px] mx-auto shadow-xl rounded-l-xl rounded-r-xl py-5 pl-5 flex items-center justify-between border-t-4 border-cyan-600 mt-3 sticky top-0">
@@ -40,17 +44,33 @@ const Header = ({ active, setActive }) => {
               About
             </Link>
           </li>
-          <li>
-            <Link
-              to="/auth"
-              className={`mx-5 ${
-                active === "signin" ? "text-cyan-500 underline" : ""
-              }`}
-              onClick={() => setActive("signin")}
-            >
-              Signin
-            </Link>
-          </li>
+          <ul className="flex items-center mx-5">
+            {userId ? (
+              <>
+                <li className="flex items-center mr-6">
+                  <span className="border rounded-full p-3 mr-3">
+                    <FaUserAlt className="text-gray-600" />
+                  </span>
+                  <span className="font-semibold">{userName[0]}</span>
+                </li>
+                <li>Logout</li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link
+                    to="/auth"
+                    className={`mx-5 ${
+                      active === "signin" ? "text-cyan-500 underline" : ""
+                    }`}
+                    onClick={() => setActive("signin")}
+                  >
+                    Signin
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
         </ul>
       </nav>
     </header>
