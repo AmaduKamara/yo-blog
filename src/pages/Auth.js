0ebf072e1f32fe2e78ad4b2dfae1d90a47a9aab4
Amkam <amaduskamara@gmail.com>
2022-11-07 09:04:42 +0000

    Add signin and signup forms conditionally

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,9 +1,98 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+
+const initialState = {
+  email: "",
+  passowrd: "",
+};
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  const [state, setState] = useState(initialState);
+  const [signUp, setSignUp] = useState(false);
 
-export default Auth
+  const { email, password } = state;
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <div className="w-[600px] mx-auto">
+      <div className="mt-16 bg-white border rounded-md p-5">
+        {!signUp ? (
+          <h1 className="text-center font-semibold text-xl md:text-2xl">
+            Sign In
+          </h1>
+        ) : (
+          <h1 className="text-center font-semibold text-xl md:text-2xl">
+            Sign Up
+          </h1>
+        )}
+        <form className="mt-10">
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={handleChange}
+              className="mb-4 py-4 px-3 rounded-md border w-full focus:outline-cyan-600"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              placeholder="Password"
+              onChange={handleChange}
+              className="mb-4 py-4 px-3 rounded-md border w-full focus:outline-cyan-600"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="py-3 rounded-md bg-cyan-500 w-full text-white uppercase hover:bg-cyan-700"
+            >
+              {!signUp ? "Signin" : "Signup"}
+            </button>
+          </div>
+        </form>
+        <div>
+          {!signUp ? (
+            <>
+              <div className="text-center mt-8">
+                <p>
+                  Don't have an account?{" "}
+                  <span
+                    className="text-orange-500 cursor-pointer"
+                    onClick={() => setSignUp(true)}
+                  >
+                    Sign Up
+                  </span>{" "}
+                </p>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="text-center mt-8">
+                <p>
+                  Already have an account?{" "}
+                  <span
+                    className="text-cyan-500 cursor-pointer"
+                    onClick={() => setSignUp(false)}
+                  >
+                    Sign In
+                  </span>{" "}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
